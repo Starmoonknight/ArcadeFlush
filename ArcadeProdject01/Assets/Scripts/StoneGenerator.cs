158ba0a5a8d1f8d799f1c02e80c37273368dd50b
Starmoonknight <44838508+Starmoonknight@users.noreply.github.com>
2019-01-09 13:24:29 +0100

    Vårt Spel
    Spelet vi jobbar med på Andres lektioner #Yolo

---
+++ ArcadeProdject01/Assets/Scripts/StoneGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneGenerator : MonoBehaviour {
+
+
+
+	public GameObject SpawnL1;
+	public GameObject SpawnL2;
+	public GameObject SpawnL3; 
+
+	public GameObject SpawnR1;
+	public GameObject SpawnR2;
+	public GameObject SpawnR3;
+
+
+	public float fallspeed;
+	public float respawnrate;
+
+
+
+	// Use this for initialization
+	void Start () {
+		print ("Hej");
+	}
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
