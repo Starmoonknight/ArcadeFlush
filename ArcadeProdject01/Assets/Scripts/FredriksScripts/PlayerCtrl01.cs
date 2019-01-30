3f34207a4271cea7f129ea51cfc49091ebc41cbc
Fredrk Noresund <fredrik_noresund@hotmail.com>
2019-01-30 13:48:46 +0100

    Worked with player controlls
    I added force to jump and set keybinds for left and right.

--- ArcadeProdject01/Assets/Scripts/FredriksScripts/PlayerCtrl01.cs
+++ ArcadeProdject01/Assets/Scripts/FredriksScripts/PlayerCtrl01.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerCtrl01 : MonoBehaviour
 {
+    public float jumpforce = 150f;
+    public Rigidbody rb;
+    public float MoveSpeed = 0.2f;
+
+    bool grounded;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>(); 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        PlayerMovement();
+    }
+
+    void PlayerMovement() 
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(MoveSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-MoveSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true) 
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpforce);
+            print("jump!");
+
+        }
     }
 }
