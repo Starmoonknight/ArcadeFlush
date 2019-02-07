867b673eef4dd9a4f7189056c003e3c7b7e5e876
Fredrk Noresund <fredrik_noresund@hotmail.com>
2019-02-07 13:42:34 +0100

    PlayerCtrls
    - Finished the player controller script.
    - Added Player controller script for player 2.
    - Made it so that jumps don't have random heights and instead is set to only one heigt.

---
+++ ArcadeProdject01/Assets/Scripts/FredriksScripts/PlayerCtrl/PlayerCtrl01.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCtrl01 : MonoBehaviour
+{
+    float jumpforce = 6f;   // The force or the speed that the player jumps at.
+    public float MoveSpeed = 0.2f;  // The speed which the player moves at.
+    bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    // PlayerMovement moves the character with the WASD keys (excluding the "S" key.
+    void PlayerMovement() 
+    {
+        if (Input.GetKey(KeyCode.D))    // If the "D" key is held down the character will move to the right.
+        {
+            transform.position += new Vector3(MoveSpeed, 0, 0); 
+        }
+
+        if (Input.GetKey(KeyCode.A))    // If the "A" key is held down the character will move to the left.
+        {
+            transform.position += new Vector3(-MoveSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && grounded == true)    // If the "W" key is pressed then the player will jump (Only when the player is "grounded")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpforce);
+            print("jump!");
+
+        }
+    }
+    // Checks if the player is grounded, making jump possible.
+    void OnTriggerStay2D(Collider2D trigger)
+    {
+       if (trigger.gameObject.CompareTag("Grounded"))
+        {
+            grounded = true;
+            print("grounded");
+        }
+
+    }
+    // Turns off grounded when the player is in the air. This prevents double jump.
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Grounded"))
+        {
+            grounded = false;
+            print("Not Grounded");
+        }
+    }
+}
---
+++ ArcadeProdject01/Assets/Scripts/FredriksScripts/PlayerCtrl/PlayerCtrl02.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerCtrl02 : MonoBehaviour
+{
+    float jumpforce = 6f;   //The force or speed that the player jump at.
+    public float MoveSpeed = 0.2f; //The speed which the player move at.
+    bool grounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    // Playermovement moves the character with the arrowkeys.
+    void PlayerMovement()   
+    {
+        if (Input.GetKey(KeyCode.RightArrow))   //If the right arrow key is held down the player will move to the right.
+        {
+            transform.position += new Vector3(MoveSpeed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))    //If the left arrow key is held down the player will move to the left.
+        {
+            transform.position += new Vector3(-MoveSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded == true)  //If the Up Arrow key is pushed the player jumps (Only works when the player is grounded.)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpforce);
+            print("jump!");
+
+        }
+    }
+    // Checks if player is grounded making jump possible.
+    void OnTriggerStay2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.CompareTag("Grounded"))
+        {
+            grounded = true;
+            print("grounded");
+        }
+
+    }
+    // Turns off grounded when character is in the air, to prevent double jump
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Grounded"))
+        {
+            grounded = false;
+            print("Not Grounded");
+        }
+    }
+}
